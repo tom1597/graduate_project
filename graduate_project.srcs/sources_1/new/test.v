f1b850dcb98abe9176b14d5c0d0b2c9ac2af1236
hyeonjin <2019103946@khu.ac.kr>
2024-03-15 17:34:26 +0900

    first

---
+++ graduate_project.srcs/sources_1/new/test.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/03/15 17:19:38
+// Design Name: 
+// Module Name: test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test(
+input a,
+output b
+    );
+assign b=a;    
+endmodule
